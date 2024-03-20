6f844455e798da2cb78adf3d2ee48a0c8192a32d
Markiian Tsalyk <m.tsalyk@ucu.edu.ua>
2024-03-20 15:13:56 +0200

    Update llm_api.py

--- llm/llm_api.py
+++ llm/llm_api.py
@@ -5,69 +5,12 @@ import uvicorn
 import requests
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from api_schema import MetadataInput, ReasoningInput
+from prompt_templates import *
 
 load_dotenv()
 
-
-METADATA_TEMPLATE = """
-SYSTEM: You are an AI movie search assistant that extracts key
-features from user queries.
-Your task is to extract information from a user query as input and returns
-a dictionary containing this information in structured way.
-The dictionary format should have the following keys:
-'title': A string representing the title of the movie.
-'genre': A string representing the genre of the movie.
-'min_year': A string representing the minimal release year of the movie.
-'max_year': A string representing the maximal release year of the movie.
-'query': The original user query.
-If any of the keys cannot be extracted from the user query, they should be
-empty strings in the returned dictionary.
-You should return only dictionary without any additional information.
-
-Few shots:
-USER: Bring me a horror movie from 2010
-AI: {{'title': '', 'genre': 'horror', 'min_year': '2010', 'max_year': '',
-'query': 'Find me a horror movie from 2010'}}
-
-USER: I don't know what to watch
-AI: {{'title': '', 'genre': '', 'min_year': '', 'max_year': '',
-'query': 'I don't know what to watch'}}
-
-USER: <query>
-AI:
-"""
-
-REASONING_TEMPLATE = """
-SYSTEM: You are an AI movie search assistant that helps to explain why certain
-movie might be relevant for the user.
-Your task is to explain relevance of the film given user query and
-reccommended movie description.
-
-Follow the guidlines:
-1. Reasoning should be clear and comprehensive
-2. It should be short and informative
-3. Do not reccommended any other movies, provie reasoning for the described
-movie
-
-Movie title: <title>
-Movie description: <description>
-
-USER: <query>
-AI:
-"""
-
 app = FastAPI()
-
-
-class MetadataInput(BaseModel):
-    query: str
-
-
-class ReasoningInput(BaseModel):
-    title: str
-    description: str
-    query: str
 
 
 @app.get("/")
